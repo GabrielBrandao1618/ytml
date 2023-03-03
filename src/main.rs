c4628686811231abc42bc4fcc757c3d5b8ee3cec
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2023-03-03 07:07:18 -0300

    Add suport to multiple children in a tag

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fmt;
 struct Tag<'a> {
     name: String,
     attributes: HashMap<&'a str, &'a str>,
-    inner: Option<Box<Tag<'a>>>
+    inner: Vec<Box<Tag<'a>>>,
 }
 
 impl fmt::Display for Tag<'static> {
@@ -14,22 +14,35 @@ impl fmt::Display for Tag<'static> {
             let fmt_prop = format!("{key} = {val}, ");
             attributes_rep.push_str(&fmt_prop);
         }
-        write!(f, "{}({})", self.name, attributes_rep)
+        let mut inner_rep = String::new();
+        for inner in &self.inner {
+            let unwraped_inner = format!("\n {}", inner);
+            inner_rep.push_str(&unwraped_inner);
+        }
+        write!(
+            f,
+            "{}({}) {{\n {}\n}}",
+            self.name, attributes_rep, inner_rep
+        )
     }
 }
 
 fn main() {
     let t1 = Tag {
         name: String::from("html"),
-        attributes: HashMap::from([
-            ("href", "http://google.com"),
-            ("color", "blue")
-        ]),
-        inner: Some(Box::new(Tag{
-            name: String::from("head"),
-            attributes: HashMap::new(),
-            inner: None
-        }))
+        attributes: HashMap::from([("href", "http://google.com"), ("color", "blue")]),
+        inner: vec![
+            Box::new(Tag {
+                name: String::from("head"),
+                attributes: HashMap::new(),
+                inner: vec![],
+            }),
+            Box::new(Tag {
+                name: String::from("title"),
+                attributes: HashMap::new(),
+                inner: Vec::new(),
+            }),
+        ],
     };
     println!("{}", t1);
 }

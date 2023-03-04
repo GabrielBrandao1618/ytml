39e07cf0ed2c6b2e4213b86174740f02539a9226
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2023-03-04 18:19:04 -0300

    Add parsing from ytml file to html file

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,12 @@ mod ast;
 mod file_output;
 mod html;
 mod ytml;
+mod file_input;
 
-use html::ast_tag_to_html;
-use ytml::ytml_doc_to_ast;
 use file_output::write_html_to_file;
+use file_input::read_file_into_ast;
 
 fn main() {
-    let raw_ytml = "html(lang = \"pt-br\") { content test } body(){ p(color = \"blue\"){}} ";
-    let result = ytml_doc_to_ast(raw_ytml);
-    for tag in &result {
-        let html_parsed = ast_tag_to_html(&tag, 0);
-        println!("{html_parsed}");
-    }
-    write_html_to_file(".out/out.html", result);
+    let file_ast = read_file_into_ast(".out/in.ytml");
+    write_html_to_file(".out/out.html", file_ast);
 }

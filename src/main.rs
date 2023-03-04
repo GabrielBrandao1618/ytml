b4016846fcc947120f4aac8075b84907eed27319
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2023-03-04 15:08:54 -0300

    Remove unnecessary code from main

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,11 @@
-use std::collections::HashMap;
-
 mod ast;
 mod html;
 mod ytml;
 
-use ast::{Tag, TagInnerElement};
-use ytml::{ytml_tag_to_ast,ytml_doc_to_ast};
 use html::ast_to_html;
+use ytml::ytml_doc_to_ast;
 
 fn main() {
-    let root = Tag {
-        name: String::from("html"),
-        attributes: HashMap::from([
-            (String::from("href"), String::from("http://google.com")),
-            (String::from("color"), String::from("blue")),
-        ]),
-        inner: vec![TagInnerElement::Tag {
-            tag: (Tag {
-                name: String::from("html"),
-                attributes: HashMap::new(),
-                inner: vec![],
-            }),
-        }],
-    };
     let raw_ytml = "html(lang = \"pt-br\") { content test } body(){ p(color = \"blue\"){}} ";
     let result = ytml_doc_to_ast(raw_ytml);
     for tag in result {

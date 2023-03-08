7a8678c00a2b8e9dae46904076317ad9a139989f
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2023-03-08 12:22:23 -0300

    Improve file structure

---
+++ src/file_handling/mod.rs
@@ -0,0 +1,20 @@
+pub mod file_input;
+pub mod file_output;
+
+use std::path::Path;
+
+use file_output::write_html_to_file;
+use file_input::read_file_into_ast;
+
+pub fn compile_ytml_file(input_path: String, output_path: Option<String>, indent: usize) -> (String, String) {
+    let actual_output_path = output_path.unwrap_or(
+        Path::new(&input_path)
+            .with_extension("html")
+            .to_str()
+            .unwrap()
+            .to_owned(),
+    );
+    let ast = read_file_into_ast(&input_path);
+    write_html_to_file(&actual_output_path, ast, indent);
+    (input_path, actual_output_path)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 mod ast;
 mod cli;
-mod file_input;
-mod file_output;
 mod html;
 mod ytml;
+mod file_handling;
 
 use clap::Parser;
 
@@ -11,8 +10,9 @@ use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
 use std::path::Path;
 use std::sync::mpsc::channel;
 
-use file_input::read_file_into_ast;
-use file_output::write_html_to_file;
+use file_handling::file_input::read_file_into_ast;
+use file_handling::file_output::write_html_to_file;
+use file_handling::compile_ytml_file;
 
 use cli::{Cli, Command};
 
@@ -82,17 +82,4 @@ fn main() -> notify::Result<()> {
             Ok(())
         }
     }
-}
-
-fn compile_ytml_file(input_path: String, output_path: Option<String>, indent: usize) -> (String, String) {
-    let actual_output_path = output_path.unwrap_or(
-        Path::new(&input_path)
-            .with_extension("html")
-            .to_str()
-            .unwrap()
-            .to_owned(),
-    );
-    let ast = read_file_into_ast(&input_path);
-    write_html_to_file(&actual_output_path, ast, indent);
-    (input_path, actual_output_path)
 }

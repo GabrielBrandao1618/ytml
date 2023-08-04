bef0c4cceddc6ca71baec7cba44275c6af0cadbd
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2023-08-04 11:57:29 -0300

    bench: improve performance with RwLock

--- benches/lang_bench.rs
+++ benches/lang_bench.rs
@@ -1,17 +1,18 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::RwLock};
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use ytml_lang::{html::ast_to_html, tokens::Tag};
 
 fn criterion_benchmark(c: &mut Criterion) {
+    let doc = RwLock::new(vec![Tag {
+        name: String::from("html"),
+        inner: Vec::new(),
+        attributes: HashMap::new(),
+    }]);
     c.bench_function("AST to html", |b| {
         b.iter(|| {
-            let doc = vec![Tag {
-                name: String::from("html"),
-                inner: Vec::new(),
-                attributes: HashMap::new(),
-            }];
-            ast_to_html(black_box(doc), black_box(0));
+            let a = doc.read().unwrap();
+            ast_to_html(black_box(a.to_vec()), black_box(0));
         })
     });
 }
